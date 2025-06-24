df36c6c69b914ce1af6544ed453960fdc23daf9e
Priyanka <priyankakumari@Priyankas-MacBook-Air.local>
2025-06-24 20:53:30 +0200

    Add page routes

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -41,19 +41,23 @@ const musicInstructors = [
   },
 ];
 
-
 const Instructors = () => {
   return (
     <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
       <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
-        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Meet Our Instructors</h2>
-        <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your musical journey</p>
+        <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">
+          Meet Our Instructors
+        </h2>
+        <p className="text-base md:text-lg text-white text-center mb-4">
+          Discover the talented professionals who will guide your musical
+          journey
+        </p>
         <div className="flex items-center justify-center mb-10 w-full">
-<AnimatedTooltip items={musicInstructors} />
+          <AnimatedTooltip items={musicInstructors} />
         </div>
       </WavyBackground>
     </div>
   );
 };
 
-export default Instructors
+export default Instructors;
